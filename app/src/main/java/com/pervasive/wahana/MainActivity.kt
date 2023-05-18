fca9f30cc1f7a92bc9c02ddf8fe4a735d2639997
agungsblh <agungsabillah123@gmail.com>
2023-05-19 01:30:06 +0800

    tes

--- app/src/main/java/com/pervasive/wahana/MainActivity.kt
+++ app/src/main/java/com/pervasive/wahana/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.pervasive.wahana
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.pervasive.wahana.activities.ScannerActivity
 import com.pervasive.wahana.databinding.ActivityMainBinding
+import com.pervasive.wahana.utils.GlobalData
+import com.pervasive.wahana.utils.LinkApi
+import com.pervasive.wahana.utils.LoadingDialog
+import com.pervasive.wahana.utils.LoadingDialogFrg
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    lateinit var sharedPreferences: SharedPreferences
+    val loading = LoadingDialog(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getDataAkun()
+        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        val editor:SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putBoolean("Login?",true)
+        editor.putString("email",GlobalData.email_user)
+        editor.putString("password",GlobalData.password)
+        editor.putInt("saldo",GlobalData.saldo)
+        editor.apply()
 
         val navController = findNavController(R.id.fragment)
         binding.bottomNavigationView.setupWithNavController(navController)
@@ -41,6 +62,39 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment)
         return navController.navigateUp()||return super.onSupportNavigateUp()
+    }
+    private fun getDataAkun(){
+        loading.startLoading()
+        var queue: RequestQueue = Volley.newRequestQueue(this)
+        var reques = JsonArrayRequest(
+            Request.Method.GET, LinkApi.link_get_data_akun+"?email="+GlobalData.email_user+"&password="+GlobalData.password,null,
+            { response ->
+                loading.isDismiss()
+                if(response.length()==0){
+
+                }else{
+
+                    for (s in 0..response.length()-1){
+                        val job = response.getJSONObject(s)
+                        GlobalData.id_user = job.getInt("id")
+                        GlobalData.nama_user = job.getString("nama")
+                        GlobalData.email_user = job.getString("email")
+                        GlobalData.password= job.getString("password")
+                        GlobalData.no_hp= job.getString("no_hp")
+                        GlobalData.id_riwayat_penyakit = job.getInt("riwayat_penyakit")
+                        GlobalData.tinggi_badan = job.getInt("tinggi_badan")
+                        GlobalData.berat_badan = job.getInt("berat_badan")
+                        GlobalData.saldo = job.getInt("saldo")
+                        GlobalData.detail_riwayat_penyakit = job.getString("nama_penyakit")
+                    }
+
+                }
+            },
+            { error ->
+                LoadingDialogFrg.hideLoading()
+                Toast.makeText(this,error.toString(), Toast.LENGTH_SHORT).show()
+            })
+        queue.add(reques)
     }
 
 }
--- app/src/main/java/com/pervasive/wahana/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/pervasive/wahana/activities/SplashScreenActivity.kt
@@ -2,27 +2,44 @@ package com.pervasive.wahana.activities
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pervasive.wahana.MainActivity
 import com.pervasive.wahana.R
 import com.pervasive.wahana.databinding.ActivitySplashScreenBinding
+import com.pervasive.wahana.utils.GlobalData
 
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashScreenBinding
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        var keepLogin = sharedPreferences.getBoolean("Login?",false)
 //        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
 //        var finishIntro = sharedPreferences.getBoolean("FinishIntro?",false)
         binding.logoapp.alpha = 0f
 
         binding.logoapp.animate().setDuration(1500).alpha(1f).withEndAction{
-            val i = Intent(this, LoginActivity::class.java)
-            startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-            finish()
+            if (keepLogin.equals(true)){
+                val i = Intent(this, MainActivity::class.java)
+                GlobalData.saldo = sharedPreferences.getInt("saldo",0)
+                GlobalData.email_user = sharedPreferences.getString("email","")!!
+                GlobalData.password = sharedPreferences.getString("password","")!!
+                startActivity(i)
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                finish()
+            }else{
+                val i = Intent(this, LoginActivity::class.java)
+                startActivity(i)
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                finish()
+            }
+
 
 
         }

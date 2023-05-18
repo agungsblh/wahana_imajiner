2fec885bdc99c143413efb1338644071a500126c
agungsblh <agungsabillah123@gmail.com>
2023-05-18 16:05:24 +0800

    tes

--- app/src/main/java/com/pervasive/wahana/MainActivity.kt
+++ app/src/main/java/com/pervasive/wahana/MainActivity.kt
@@ -2,6 +2,9 @@ package com.pervasive.wahana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.pervasive.wahana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +14,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
+        val navController = findNavController(R.id.fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.berandaFragment
+                ||destination.id == R.id.akunFragment) {
+                binding.bottomAppBar.visibility = View.VISIBLE
+                binding.scanner.visibility = View.VISIBLE
+            } else {
+                binding.scanner.visibility = View.GONE
+                binding.bottomAppBar.visibility = View.GONE
+            }
+        }
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp()||return super.onSupportNavigateUp()
+    }
+
 }

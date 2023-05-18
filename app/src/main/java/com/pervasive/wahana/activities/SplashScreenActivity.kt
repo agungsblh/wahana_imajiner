a86cd39680fcf8168515f019d836cf679f975b01
agungsblh <agungsabillah123@gmail.com>
2023-05-18 17:05:59 +0800

    tes

---
+++ app/src/main/java/com/pervasive/wahana/activities/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.pervasive.wahana.activities
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pervasive.wahana.R
+import com.pervasive.wahana.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+//        var finishIntro = sharedPreferences.getBoolean("FinishIntro?",false)
+        binding.logoapp.alpha = 0f
+
+        binding.logoapp.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, RegisterActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+
+
+        }
+    }
+}

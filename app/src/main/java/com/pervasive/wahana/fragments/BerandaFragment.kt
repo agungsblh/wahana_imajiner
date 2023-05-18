d3c1786db4033d9da600f04b1b2cfd891baebd8d
agungsblh <agungsabillah123@gmail.com>
2023-05-18 19:45:26 +0800

    tes

--- app/src/main/java/com/pervasive/wahana/fragments/BerandaFragment.kt
+++ app/src/main/java/com/pervasive/wahana/fragments/BerandaFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.pervasive.wahana.R
 import com.pervasive.wahana.databinding.FragmentBerandaBinding
 
@@ -22,7 +23,9 @@ class BerandaFragment : Fragment() {
     }
     private fun onAction(){
         binding.apply {
-
+            topUp.setOnClickListener {
+                findNavController().navigate(R.id.action_berandaFragment_to_topupFragment)
+            }
         }
     }
 }
--- app/src/main/java/com/pervasive/wahana/fragments/TopupFragment.kt
+++ app/src/main/java/com/pervasive/wahana/fragments/TopupFragment.kt
@@ -13,18 +13,39 @@ import com.pervasive.wahana.databinding.FragmentTopupBinding
 class TopupFragment : Fragment() {
     private var _binding : FragmentTopupBinding?= null
     private val binding get() = _binding!!
+    private var tp_lv1 = 0
+    private var tp_lv2 = 0
+    private var tp_lv3 = 0
+    private var tp_lv4 = 0
+    private var tp_lv5 = 0
+    private var tp_lv6 = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentTopupBinding.inflate(inflater, container, false)
         onAction()
 
-
+        topup_custombox()
 
         return binding.root
     }
     private fun onAction(){
         binding.apply {
-
+            btnBack.setOnClickListener {
+                requireActivity().onBackPressed()
+            }
+        }
+    }
+    private fun topup_custombox(){
+        binding.apply {
+            lv1.setOnClickListener {
+                tp_lv1 = 1
+                tp_lv2 = 0
+                tp_lv3 = 0
+                tp_lv4 = 0
+                tp_lv5 = 0
+                tp_lv6 = 0
+                lv1.setCardBackgroundColor(resources.getColor(R.color.hijau_soft))
+            }
         }
     }
 }

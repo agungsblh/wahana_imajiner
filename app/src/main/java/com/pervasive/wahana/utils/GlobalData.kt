08801ba5e890b869dc37584f0189c0f000f7177e
agungsblh <agungsabillah123@gmail.com>
2023-05-18 19:55:39 +0800

    tes

---
+++ app/src/main/java/com/pervasive/wahana/utils/GlobalData.kt
@@ -0,0 +1,16 @@
+package com.pervasive.wahana.utils
+
+class GlobalData {
+    companion object{
+        var id_user:Int = 0
+        var saldo:Int = 0
+        var nama_user:String = String()
+        var foto_profil:String = String()
+        var email_user:String = String()
+        var no_hp:String = String()
+        var jenis_kelamin:String = String()
+        var riwayat_penyakit:String = String()
+        var tinggi_badan:Int = 0
+        var berat_badan:Int = 0
+    }
+}

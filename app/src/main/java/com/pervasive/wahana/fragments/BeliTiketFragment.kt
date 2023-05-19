78d3d9cf2f8589b5d0f2d6b0f7d5202e5d92e20e
agungsblh <agungsabillah123@gmail.com>
2023-05-20 01:32:30 +0800

    tes

--- app/src/main/java/com/pervasive/wahana/fragments/BeliTiketFragment.kt
+++ app/src/main/java/com/pervasive/wahana/fragments/BeliTiketFragment.kt
@@ -1,60 +1,30 @@
 package com.pervasive.wahana.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pervasive.wahana.R
+import com.pervasive.wahana.databinding.FragmentAkunBinding
+import com.pervasive.wahana.databinding.FragmentBeliTiketBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BeliTiketFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BeliTiketFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding : FragmentBeliTiketBinding?= null
+    private val binding get() = _binding!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentBeliTiketBinding.inflate(inflater, container, false)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        onAction()
+
+        return binding.root
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beli_tiket, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BeliTiketFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BeliTiketFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun onAction(){
+        binding.apply {
+            back.setOnClickListener {
+                requireActivity().onBackPressed()
             }
+        }
     }
 }
